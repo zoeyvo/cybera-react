2e9ee0174b8c6c830ca739f1e8f30b5ccc90259c
zoeyvo <zoeyvo256@gmail.com>
2025-05-30 04:23:05 -0700

    more route fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './App.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>,
