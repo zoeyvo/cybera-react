ff0672b6e3ecb2ed995d032e881a9466107cc993
zoeyvo <zoeyvo256@gmail.com>
2025-05-30 04:40:19 -0700

    changing routing method

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './App.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
