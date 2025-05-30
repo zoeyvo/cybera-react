e7e10f517ae5a42efbe51f4ff41bb7e6eb369250
zoeyvo <zoeyvo256@gmail.com>
2025-05-30 04:12:48 -0700

    MIME error fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/cyberia/', // Set to your repo name if your repo is github.com/zoeyvo/cyberia
   plugins: [react()],
   build: {
     outDir: 'docs',
