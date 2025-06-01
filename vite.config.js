a83a17073533af4c80d09a48f16ea94aa59891c2
zoeyvo <zoeyvo256@gmail.com>
2025-06-01 02:49:31 -0700

    final final stlying

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // Use relative paths for all built assets
+  build: {
+    outDir: 'docs', // Output build to docs/ for GitHub Pages
+  },
 })
