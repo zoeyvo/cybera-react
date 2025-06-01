1007ca55d7b8dd7f0e4b4c7508e2b05221db8d62
zoeyvo <zoeyvo256@gmail.com>
2025-06-01 03:45:51 -0700

    i learned how to test with npx serve docs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Use process.env.BASE or default to './' for local preview
+const base = process.env.BASE || './';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for all built assets
+  base,
   build: {
     outDir: 'docs', // Output build to docs/ for GitHub Pages
   },
-})
+});
